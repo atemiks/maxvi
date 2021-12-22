4259f2169ed46e5df3e8c7a71c0b0c7ffd568119
atemiks <atemiks@gmail.com>
2021-12-22 16:21:21 +0200

    Maxvi search update

--- search/search.js
+++ search/search.js
@@ -2,20 +2,29 @@ let headerSearchToggle = $('.header-search-toggle'),
     headerSearchContainer = $('.header-search-overlay'),
     headerSearchInput = $(headerSearchContainer).find('.header-search-input'),
     headerSearchSuggestionList = $(headerSearchContainer).find('.header-search-suggestion-list')
-    headerSearchBg = $(headerSearchContainer).find('.header-search-backdrop');
+    headerSearchBg = $(headerSearchContainer).find('.header-search-backdrop'),
+    headerNavigationToggle = $('.header-toggle');
 
 function showSearch() {
+    $(headerNavigationToggle).addClass('hide');
+    $(headerSearchToggle).addClass('active');
     $(headerSearchContainer).addClass('show');
     $(headerSearchInput).focus();
 }
 
 function hideSearch() {
+    $(headerNavigationToggle).removeClass('hide');
+    $(headerSearchToggle).removeClass('active');
     $(headerSearchContainer).removeClass('show');
     $(headerSearchInput).blur();
 }
 
 $(headerSearchToggle).on('click', function() {
-    showSearch();
+    if($(headerSearchToggle).hasClass('active')) {
+        hideSearch();
+    } else {
+        showSearch();
+    }
 });
 
 $(headerSearchBg).on('click', function() {
