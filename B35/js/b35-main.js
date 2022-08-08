929b91c25000898fbbd10e98521b1b5d437a0856
atemiks <atemiks@gmail.com>
2022-08-08 20:56:08 +0300

    Maxvi B35 added battery animation

--- B35/js/b35-main.js
+++ B35/js/b35-main.js
@@ -5,4 +5,44 @@ $(document).ready(function() {
     		anchorPlacement: 'top-bottom'
     	});
     }, 100);
+
+
+	// battery number counter
+	function animateNumber(elem, endNumber, inter, steps) {
+		let element = document.querySelector('.' + elem);
+		let startCount = +element.innerHTML;
+		let delay = Math.round(inter / (endNumber / steps));
+
+		let interval = setInterval(() => {
+			startCount += steps;
+
+			if (startCount > endNumber) {
+				startCount = endNumber
+			};
+
+			if (startCount == endNumber) {
+				clearInterval(interval)
+			};
+
+			element.innerHTML = startCount;
+		}, delay);
+	}
+
+	function scrollNumberAnimation(elem, endNumber, inter, steps, defaultNumber) {
+		let element = document.querySelector('.' + elem);
+		let onTop = (element.getBoundingClientRect().y + (element.offsetHeight * 0.5)) - document.documentElement.clientHeight;
+		let onBottom = (element.getBoundingClientRect().y + element.offsetHeight);
+		
+		
+		if (onTop < 0 && onBottom > 0 && +element.innerHTML !== endNumber && !(+element.innerHTML < endNumber && +element.innerHTML > defaultNumber)) {
+			animateNumber(elem, endNumber, inter, steps)
+		}
+		else if (+element.innerHTML !== defaultNumber && !(onTop < 0 && onBottom > 0) && !(+element.innerHTML < endNumber && +element.innerHTML > defaultNumber)){
+			element.innerHTML = defaultNumber;
+		};
+	}
+
+	window.addEventListener('scroll', function() {
+		scrollNumberAnimation('b35-battery-counter', 2500, 1500, 100, 0);
+	});
 });
