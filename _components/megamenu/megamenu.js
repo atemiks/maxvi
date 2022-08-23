ff8654c3d2bd683e0c9969681953edc64aeb65c5
atemiks <atemiks@gmail.com>
2022-08-23 19:05:18 +0300

    Maxvi megamenu component update

--- _components/megamenu/megamenu.js
+++ _components/megamenu/megamenu.js
@@ -2,7 +2,9 @@ let megamenuToggle = $( ".header-megamenu-toggle" ),
     megamenuContainer = $( ".megamenu-wrapper" ),
     megamenuNavContainer = $('.megamenu-nav-list'),
     megamenuNavChildren = $(megamenuNavContainer).find('.megamenu-nav-item'),
-    activeSubnavContainer = undefined;
+    megamenuPaneContainers = $(megamenuContainer).find('.megamenu-tab-pane');
+
+    console.log(megamenuPaneContainers);
 
 function openMegamenu() {
     $(megamenuToggle).addClass('active');
@@ -15,40 +17,35 @@ function closeMegamenu() {
 }
 
 
-function setMegamenuHeight(height) {
-    $(".megamenu-nav-list").css("min-height", height);
-}
-
 
 $(megamenuToggle).on('click', function() {
     $(this).hasClass('active') ? closeMegamenu() : openMegamenu();
 });
 
 $(document).on('click', function(e) {
-    if ( !megamenuToggle.is(e.target) && megamenuToggle.has(e.target).length === 0 && !megamenuNavContainer.is(e.target) && megamenuNavContainer.has(e.target).length === 0 ) {
+    let megamenuContentContainer = $(megamenuContainer).find('.megamenu-inner');
+
+    if ( !megamenuToggle.is(e.target) && megamenuToggle.has(e.target).length === 0 && !megamenuContentContainer.is(e.target) && megamenuContentContainer.has(e.target).length === 0 ) {
         closeMegamenu();
     }
 });
 
-$('.megamenu-nav-item').hover(
-    function () {
-        let target = $(this);
-        
-        $(megamenuNavChildren).each(function(index) {
-            $(megamenuNavChildren[index]).removeClass('active');
-        });
 
-        $(target).addClass('active');
-        activeSubnavContainer = $(target).find($('.megamenu-subnav-wrapper'));
-        setMegamenuHeight(activeSubnavContainer.outerHeight());
+$('.megamenu-nav-list .megamenu-nav-link').hover(function(e) {
+    let target = $(this);
+
+    if(!($(target)[0].hasAttribute('data-toggle'))) {
+        $(megamenuNavChildren).removeClass('active');
+        $(megamenuPaneContainers).removeClass('active');
+    } else {
+        $(target).tab('show');
     }
-);
+});
 
-$('.megamenu-subnav-toggle').on('click', function() {
+
+$('.megamenu-pane-toggle').on('click', function() {
     let target = $(this);
 
     $(target).toggleClass('active');
-    $(target).next().slideToggle('fast', function() {
-        setMegamenuHeight(activeSubnavContainer.outerHeight());
-    });
+    $(target).next().slideToggle('fast');
 })
