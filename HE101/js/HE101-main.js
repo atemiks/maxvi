18cd1766dd75d43da876e3d12f7d47093cf192e1
atemiks <atemiks@gmail.com>
2023-02-02 19:40:17 +0200

    Maxi HE101 update

--- HE101/js/HE101-main.js
+++ HE101/js/HE101-main.js
@@ -2,7 +2,37 @@ $(document).ready(function () {
     //animation
     setTimeout(function () {
         AOS.init({
-            anchorPlacement: 'top-bottom',
+            anchorPlacement: "top-bottom",
         });
     }, 100);
+
+    const HE101IndicatiorAnimate = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".HE101-indicator",
+            startAnimation: "bottom bottom",
+            start: "0",
+            end: "50%",
+            toggleActions: "restart none none none",
+            scrub: 3,
+        },
+    });
+
+    const HE101ModeAnimate = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".HE101-mode",
+            startAnimation: "bottom bottom",
+            start: "0",
+            end: "50%",
+            toggleActions: "restart none none none",
+            scrub: 3,
+        },
+    });
+
+    HE101IndicatiorAnimate.to(".HE101-indicator .HE101-media-active", {
+        opacity: 1,
+    });
+
+    HE101IndicatiorAnimate.to(".HE101-mode .HE101-media-active", {
+        opacity: 1,
+    });
 });
