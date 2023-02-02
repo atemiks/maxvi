21d830d9f2c3d89416094a9b789c4435adf646ce
atemiks <atemiks@gmail.com>
2023-02-02 19:43:31 +0200

    Maxvi HE111 update

--- HE111/js/HE111-main.js
+++ HE111/js/HE111-main.js
@@ -2,7 +2,37 @@ $(document).ready(function () {
     //animation
     setTimeout(function () {
         AOS.init({
-            anchorPlacement: 'top-bottom',
+            anchorPlacement: "top-bottom",
         });
     }, 100);
+
+    const HE111IndicatiorAnimate = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".HE111-indicator",
+            startAnimation: "bottom bottom",
+            start: "0",
+            end: "50%",
+            toggleActions: "restart none none none",
+            scrub: 3,
+        },
+    });
+
+    const HE111ModeAnimate = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".HE111-mode",
+            startAnimation: "bottom bottom",
+            start: "0",
+            end: "50%",
+            toggleActions: "restart none none none",
+            scrub: 3,
+        },
+    });
+
+    HE111IndicatiorAnimate.to(".HE111-indicator .HE111-media-active", {
+        opacity: 1,
+    });
+
+    HE111IndicatiorAnimate.to(".HE111-mode .HE111-media-active", {
+        opacity: 1,
+    });
 });
