c505c039be3e56f37140666f1f75e1fa0c2f419e
atemiks <atemiks@gmail.com>
2023-03-14 15:39:55 +0200

    Maxvi HD1801

--- HD1801/js/HD1801-main.js
+++ HD1801/js/HD1801-main.js
@@ -13,49 +13,61 @@ $(document).ready(function () {
     const HD1801ModeScene = $('#HD1801-mode-parallax').get(0);
 	const HD1801ModeInstance = new Parallax(HD1801ModeScene);
 
-    // scroll animation 
-    const toggleActiveImages = (ctx, arrayOfImages) => {
-        arrayOfImages.forEach((image, index) => {
-            if(ctx !== index) {
-                image.classList.remove('active');
-            }
-        })
+    // scroll animation
+    const HD1801InitDesignAnimation = () => {
+        const toggleActiveImages = (ctx, arrayOfImages) => {
+            arrayOfImages.forEach((image, index) => {
+                if(ctx !== index) {
+                    image.classList.remove('active');
+                }
+            })
+        }
+        
+        const HD1801DesignImages = document.querySelectorAll('.HD1801-design .HD1801-media-figure-desktop .HD1801-media-layer');
+    
+        HD1801DesignImages.forEach((image, index, array) => {
+            gsap.to(image, {
+                scrollTrigger: {
+                    trigger: '.HD1801-design',
+                    start: `${index * 1.5 + 50}% 100%`, 
+                    end: '+=0 100%',
+                    scrub: true,
+                    markers: false,
+                    onEnter: () => {
+                        image.classList.add('active');
+                        toggleActiveImages(index, array);
+                    },
+                    onEnterBack: () => {
+                        image.classList.add('active');
+                        toggleActiveImages(index, array);
+                    },
+                },
+                opacity: '1',
+                visibility: 'visible',
+            });
+        });
+    
+        const myAnimation = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".HD1801-design",
+                toggleActions: "restart none none none",
+                start: "75% 100%",
+                end: "100% 100%",
+                scrub: 0.1,
+                markers: false,
+            },
+        }).to(".HD1801-design .HD1801-text-block", {
+            width: '400px',
+        });
     }
 
-    const HD1801DesignImages = document.querySelectorAll('.HD1801-design .HD1801-media-figure .HD1801-media-layer');
+    if(window.innerWidth >= 768) {
+        HD1801InitDesignAnimation();
+    }
 
-    HD1801DesignImages.forEach((image, index, array) => {
-        gsap.to(image, {
-            scrollTrigger: {
-                trigger: '.HD1801-design',
-                start: `${index * 1.5 + 50}% 100%`, 
-                end: '+=0 100%',
-                scrub: true,
-                markers: false,
-                onEnter: () => {
-                    image.classList.add('active');
-                    toggleActiveImages(index, array);
-                },
-                onEnterBack: () => {
-                    image.classList.add('active');
-                    toggleActiveImages(index, array);
-                },
-            },
-            opacity: '1',
-            visibility: 'visible',
-        });
-    });
-
-    gsap.timeline({
-        scrollTrigger: {
-            trigger: ".HD1801-design",
-            toggleActions: "restart none none none",
-            start: "75% 100%",
-            end: "100% 100%",
-            scrub: 0.1,
-            markers: false,
-        },
-    }).to(".HD1801-design .HD1801-text-block", {
-        width: '400px',
+    window.matchMedia('(min-width: 768px)').addEventListener('change', (event) => {
+        if(event.matches) {
+            HD1801InitDesignAnimation();
+        }
     });
 });
