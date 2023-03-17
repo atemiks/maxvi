30a468ddd051eb5f146a3956be5b08e3683ec810
atemiks <atemiks@gmail.com>
2023-03-17 22:43:23 +0200

    Maxvi KE1701P

---
+++ KE1701P/js/KE1701P-main.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+    //animation
+    setTimeout(function () {
+        AOS.init({
+            anchorPlacement: 'top-bottom',
+        });
+    }, 100);
+
+    // parallax
+    const KE1701PFilterScene = $('#KE1701P-filter-parallax').get(0);
+    const KE1701PFilterInstance = new Parallax(KE1701PFilterScene);
+
+    const toggleActiveImages = (image, index, arrayOfImages) => {
+        image.classList.add('active');
+        
+        arrayOfImages.forEach((image, idx) => {
+            if(index !== idx) {
+                image.classList.remove('active');
+            }
+        })
+    }
+
+    // hover intro section animation
+    const KE1701PIntroImages = document.querySelectorAll('.KE1701P-intro .KE1701P-media-layers .KE1701P-media-layer');
+    const KE1701PIntroPaginationItems = document.querySelectorAll('.KE1701P-intro .KE1701P-media-pagination .KE1701P-media-layer');
+
+    KE1701PIntroPaginationItems.forEach((paginationItem, index) => {
+        paginationItem.setAttribute('data-index', index);
+        paginationItem.addEventListener('mouseover', (e) => {
+            const currentIndex = Number(e.target.getAttribute('data-index'));
+            toggleActiveImages(KE1701PIntroImages[currentIndex], currentIndex, KE1701PIntroImages);
+        });
+    });
+
+    // scroll design section animation
+    const KE1701PDesignImages = document.querySelectorAll('.KE1701P-design .KE1701P-media-figure .KE1701P-media-layer');
+    
+    KE1701PDesignImages.forEach((image, index, array) => {
+        gsap.to(image, {
+            scrollTrigger: {
+                trigger: '.KE1701P-design',
+                start: `${index * 1.5 + 25}% 50%`, 
+                end: '+=0 50%',
+                scrub: true,
+                markers: false,
+                onEnter: () => {
+                    toggleActiveImages(image, index, array);
+                },
+                onEnterBack: () => {
+                    toggleActiveImages(image, index, array);
+                },
+            },
+            opacity: '1',
+            visibility: 'visible',
+        });
+    });
+});
