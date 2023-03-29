48601fac144af78379a5e21cc8232439285333eb
atemiks <atemiks@gmail.com>
2023-03-29 17:08:59 +0300

    Maxvi E9

--- E9/js/E9-main.js
+++ E9/js/E9-main.js
@@ -10,31 +10,39 @@ $(document).ready(function () {
     const E9IntroScene = document.getElementById('E9-intro-parallax');
     const E9IntroInstance = new Parallax(E9IntroScene);
 
-    // E9 indication scroll animation
+    // E9 indication animation
     const E9Indication = document.querySelector('.E9-indication');
     const E9IndicationBlocks = E9Indication.querySelectorAll('.E9-indication-block');
+    let E9IndicationIndexBlock = 0;
+    let E9IndicationIsIntersecting = false;
 
-    const E9IndicationToggleActiveBlock = (block, blocks) => {
-        blocks.forEach((block) => block.classList.remove('active'));
-        block.classList.add('active')
+    const E9IndicationBlocksAnimation = () => {
+        if(E9IndicationIsIntersecting) {
+            E9IndicationBlocks[E9IndicationIndexBlock].classList.remove('active');
+            E9IndicationIndexBlock++;
+            
+            if(E9IndicationIndexBlock > E9IndicationBlocks.length - 1) {
+                E9IndicationIndexBlock = 0;
+            }
+
+            E9IndicationBlocks[E9IndicationIndexBlock].classList.add('active');
+            
+            setTimeout(E9IndicationBlocksAnimation, 1800);
+        }
     }
 
-    E9IndicationBlocks.forEach((block, index, array) => {
-        gsap.to(block, {
-            scrollTrigger: {
-                trigger: E9Indication,
-                start: `${index * 5 + 50}% 50%`,
-                end: '+=0 50%',
-                scrub: true,
-                markers: false,
-                toggleClass: 'active',
-                onEnter: () => {
-                    E9IndicationToggleActiveBlock(block, array);
-                },
-                onLeaveBack: () => {
-                    E9IndicationToggleActiveBlock(block, array);
-                },
+    const E9IndicationObserver = new IntersectionObserver((entries, observe) => {
+        entries.forEach((entry) => {
+            if(entry.isIntersecting) {
+                E9IndicationIsIntersecting = true;
+                setTimeout(E9IndicationBlocksAnimation, 1800)
+            } else {
+                E9IndicationIsIntersecting = false;
             }
         });
+    }, {
+        rootMargin: '0% 0% 0%',
     });
+
+    E9IndicationObserver.observe(E9Indication);
 });
