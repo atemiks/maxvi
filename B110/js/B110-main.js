d803eeea0b34adf5e7d5f522558f1e496e5dca52
atemiks <atemiks@gmail.com>
2023-04-08 14:24:29 +0300

    Maxvi B110

---
+++ B110/js/B110-main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    //animation
+    setTimeout(function () {
+        AOS.init({
+            anchorPlacement: 'top-bottom',
+        });
+    }, 100);
+
+    // parallax
+    // const B110IntroScene = document.getElementById('B110-intro-parallax');
+    // const B110IntroInstance = new Parallax(B110IntroScene);
+});
