af9d2a133a3ab6f087a11f7e2d01a5fdfb05f57d
atemiks <atemiks@gmail.com>
2023-09-18 15:31:19 +0300

    Maxvi PR 03 gsap update, mobile animation fix

--- PR-03/files/js/main.js
+++ PR-03/files/js/main.js
@@ -21,118 +21,127 @@ $(document).ready(function () {
         '.PR03-controls-block'
     );
 
-    const PR03Timeline = gsap.timeline({
-        scrollTrigger: {
-            id: 'PR03TimelineTrigger',
-            trigger: PR03TimelineSection,
-            scrub: true,
-            start: '0% 0%',
-            end: '400% 100%',
-            snap: {
-                snapTo: [0, 0.1, 0.5, 0.8, 1], // snap to the progress in the timeline
-                duration: { min: 0.1, max: 2 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-                ease: 'power3.easeOut', // the ease of the snap animation ("power3" by default)
+    const gsapMatchMedia = gsap.matchMedia();
+    gsapMatchMedia.add({
+        isDesktop: '(min-width: 992px)',
+        isMobile: '(max-width: 991px)'
+    }, (context) => {
+        const { isDesktop } = context.conditions;
+
+        const PR03Timeline = gsap.timeline({
+            scrollTrigger: {
+                id: 'PR03TimelineTrigger',
+                trigger: PR03TimelineSection,
+                scrub: true,
+                start: '0% 0%',
+                end: '400% 100%',
+                snap: {
+                    snapTo: [0, 0.1, 0.5, 0.8, 1], // snap to the progress in the timeline
+                    duration: { min: 0.1, max: 2 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+                    ease: 'power3.easeOut', // the ease of the snap animation ("power3" by default)
+                },
+                // markers: true,
             },
-            // markers: true,
-        },
+        });
+    
+        const PR03IntroFigureTl = gsap
+            .timeline({
+                scrollTrigger: {
+                    id: 'PR03IntroFigureTrigger',
+                    trigger: PR03IntroSection,
+                    scrub: true,
+                    start: '0% 0%',
+                    end: '300% 100%',
+                    pin: PR03IntroSection,
+                    // markers: true,
+                },
+            })
+            .to(PR03IntroFigure, {
+                delay: 5,
+                duration: 5,
+                top: '42vh',
+                left: isDesktop ? '30vw' : '15vw',
+                scale: isDesktop ? 1.45 : 1.15,
+            })
+            .to(PR03IntroFigure, {
+                delay: 7,
+                duration: 3,
+                left: '100vw',
+            });
+    
+        const PR03IntroTextTl = gsap
+            .timeline({
+                scrollTrigger: {
+                    id: 'PR03IntroTextTrigger',
+                    trigger: PR03IntroSection,
+                    scrub: true,
+                    start: '0% 0%',
+                    // markers: true,
+                    end: '300% 100%',
+                    pin: PR03IntroSection,
+                },
+            })
+            .to(PR03IntroFmBlock, {
+                delay: 5,
+                duration: 5,
+                opacity: 0,
+                transform: 'translateY(-200px)',
+            })
+            .to(PR03IntroAntennaBlock, {
+                delay: 5,
+                duration: 5,
+                opacity: 1,
+                transform: 'translateY(0)',
+            })
+            .to(PR03IntroAntennaBlock, {
+                delay: 5,
+                duration: 5,
+                opacity: 0,
+                transform: 'translateY(-200px)',
+            });
+    
+        const PR03ControlsFigureTl = gsap
+            .timeline({
+                scrollTrigger: {
+                    id: 'PR03ControlsFigureTrigger',
+                    trigger: PR03ControlsSection,
+                    scrub: true,
+                    start: '0% 0%',
+                    end: '200% 100%',
+                    pin: PR03ControlsSection,
+                    // markers: true,
+                },
+            })
+            .to(PR03ControlsFigure, {
+                duration: 5,
+                opacity: 1,
+                transform: 'translateY(0)',
+            })
+            .to(PR03ControlsFigure, {
+                delay: 20,
+            });
+    
+        const PR03ControlsTextTl = gsap
+            .timeline({
+                scrollTrigger: {
+                    id: 'PR03ControlsTextTrigger',
+                    trigger: PR03ControlsSection,
+                    scrub: true,
+                    start: '0% 0%',
+                    end: '200% 100%',
+                    pin: PR03ControlsSection,
+                    // markers: true,
+                },
+            })
+            .to(PR03ControlsBlock, {
+                delay: 5,
+                duration: 5,
+                opacity: 1,
+                transform: 'translateY(0)',
+            })
+            .to(PR03ControlsBlock, {
+                delay: 20,
+            });
     });
 
-    const PR03IntroFigureTl = gsap
-        .timeline({
-            scrollTrigger: {
-                id: 'PR03IntroFigureTrigger',
-                trigger: PR03IntroSection,
-                scrub: true,
-                start: '0% 0%',
-                end: '300% 100%',
-                pin: PR03IntroSection,
-                // markers: true,
-            },
-        })
-        .to(PR03IntroFigure, {
-            delay: 5,
-            duration: 5,
-            top: '42vh',
-            left: '30vw',
-            scale: 1.45,
-        })
-        .to(PR03IntroFigure, {
-            delay: 7,
-            duration: 3,
-            left: '100vw',
-        });
-
-    const PR03IntroTextTl = gsap
-        .timeline({
-            scrollTrigger: {
-                id: 'PR03IntroTextTrigger',
-                trigger: PR03IntroSection,
-                scrub: true,
-                start: '0% 0%',
-                // markers: true,
-                end: '300% 100%',
-                pin: PR03IntroSection,
-            },
-        })
-        .to(PR03IntroFmBlock, {
-            delay: 5,
-            duration: 5,
-            opacity: 0,
-            transform: 'translateY(-200px)',
-        })
-        .to(PR03IntroAntennaBlock, {
-            delay: 5,
-            duration: 5,
-            opacity: 1,
-            transform: 'translateY(0)',
-        })
-        .to(PR03IntroAntennaBlock, {
-            delay: 5,
-            duration: 5,
-            opacity: 0,
-            transform: 'translateY(-200px)',
-        });
-
-    const PR03ControlsFigureTl = gsap
-        .timeline({
-            scrollTrigger: {
-                id: 'PR03ControlsFigureTrigger',
-                trigger: PR03ControlsSection,
-                scrub: true,
-                start: '0% 0%',
-                end: '200% 100%',
-                pin: PR03ControlsSection,
-                // markers: true,
-            },
-        })
-        .to(PR03ControlsFigure, {
-            duration: 5,
-            opacity: 1,
-            transform: 'translateY(0)',
-        })
-        .to(PR03ControlsFigure, {
-            delay: 20,
-        });
-
-    const PR03ControlsTextTl = gsap
-        .timeline({
-            scrollTrigger: {
-                id: 'PR03ControlsTextTrigger',
-                trigger: PR03ControlsSection,
-                scrub: true,
-                start: '0% 0%',
-                end: '200% 100%',
-                pin: PR03ControlsSection,
-                // markers: true,
-            },
-        })
-        .to(PR03ControlsBlock, {
-            delay: 5,
-            duration: 5,
-            opacity: 1,
-            transform: 'translateY(0)',
-        })
-        .to(PR03ControlsBlock, {
-            delay: 20,
-        });
 });
