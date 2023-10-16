5223ae5a2cb1c80c6219ad8a0ae1e6f891d64606
atemiks <atemiks@gmail.com>
2023-10-16 19:02:24 +0300

    Maxvi E10

--- E10/files/js/main.js
+++ E10/files/js/main.js
@@ -14,10 +14,6 @@ $(document).ready(function () {
     new Parallax(E10ScreenFigure);
 
     /* GSAP */
-    const E10SpeakerSection =  document.querySelector('.E10-speaker');
-    const E10SpeakerPhone = E10SpeakerSection.querySelector('.E10-speaker-figure');
-    const E10CameraSection = document.querySelector('.E10-camera');
-    const E10CameraTitle = E10CameraSection.querySelector('.E10-camera-title');
     const gsapMatchMedia = gsap.matchMedia();
 
     gsapMatchMedia.add({
@@ -27,40 +23,27 @@ $(document).ready(function () {
     }, (context) => {
         const { isPhone } = context.conditions;
 
-        gsap.to(E10SpeakerPhone, {
-            x: isPhone ? '50%': '',
-            y: isPhone ? '-50px' : '',
-            rotate: 90,
+        const timeline = gsap.timeline({
             scrollTrigger: {
-                id: 'E10SpeakerPhoneTrigger',
-                trigger: E10SpeakerSection,
+                id: 'E10SDesignPhoneTrigger',
+                trigger: E10DesignSection,
                 scrub: true,
-                start: '0% 0%',
-                pin: E10SpeakerPhone ,
-                pinSpacing: false,
+                start: isPhone ? '25% 0%' : '15% 0%',
+                end: isPhone ? '50% 0%' : '100% 0%',
+                pin: E10DesignFigure,
                 snap: {
-                    snapTo: [0, 1], // snap to the progress in the timeline
-                    duration: { min: 0.1, max: 2 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-                    ease: 'power3.easeOut', // the ease of the snap animation ("power3" by default)
+                    snapTo: "labels", // snap to the closest label in the timeline
+                    duration: { min: 0.2, max: 2 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+                    delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+                    ease: "power1.inOut", // the ease of the snap animation ("power3" by default)
                 },
                 markers: false,
             },
-        });
-
-        gsap.fromTo(E10CameraTitle, {
-            opacity: 0,
-            x: isPhone ? '-100px' : '100px',
-        }, {
-            opacity: 1,
-            x: 0,
-            scrollTrigger: {
-                id: 'E10CameraTitleTrigger',
-                trigger: E10CameraSection,
-                scrub: true,
-                start: '25% 50%',
-                end: '50% 50%',
-                markers: false,
-            },
         })
+        .addLabel('start')
+        .to(E10DesignFigure, {
+            rotate: 8
+        })
+        .addLabel('end');
     });
 });
---
+++ E10/include.php
@@ -0,0 +1,240 @@
+<?php
+
+/**
+ * Bitrix vars
+ *
+ * @var array $templateFolder
+ * @var array $arParams
+ * @var CMain $APPLICATION
+ * @var CUser $USER
+ */
+
+$folder = substr(DIR, strlen($_SERVER['DOCUMENT_ROOT'])) . '/files/';
+
+// Bootstrap 3.4.1
+$this->addExternalCss("https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css");
+
+//AOS
+$this->addExternalCss(SITE_TEMPLATE_PATH . "/css/aos.css");
+$this->addExternalJS(SITE_TEMPLATE_PATH . '/js/aos.js');
+
+//Styles
+$this->addExternalCss($folder . "css/style.css");
+$this->addExternalCss($folder . "css/style-responsive.css");
+
+//parallax
+$this->addExternalJS(SITE_TEMPLATE_PATH . '/js/parallax.min.js');
+
+// Анимация
+$this->addExternalJS(SITE_TEMPLATE_PATH . '/js/gsap3/gsap.min.js');
+$this->addExternalJS(SITE_TEMPLATE_PATH . '/js/gsap3/ScrollTrigger.js');
+
+//Scripts
+$this->addExternalJS($folder . 'js/main.js');
+
+?>
+<div class="E10-landing">
+    <section
+        class="E10-design"
+        style="
+            background-image: url('<?= $folder ?>/images/section-design/E10-design-bg.jpg');
+        "
+    >
+        <div class="container">
+            <div class="content-wrapper row d-flex">
+                <div
+                    class="E10-typography-container col-xs-12 col-sm-6"
+                >
+                    <div
+                        class="E10-design-title"
+                        data-aos="fade-right"
+                        data-aos-duration="500"
+                    >
+                        БРУТАЛЬНЫЙ
+                        <br />
+                        ДИЗАЙН
+                    </div>
+
+                    <div
+                        class="E10-design-model"
+                        data-aos="fade-right"
+                        data-aos-duration="500"
+                        data-aos-delay="100"
+                    >
+                        <img
+                            loading="lazy"
+                            src="<?= $folder ?>/images/logo-maxvi.svg"
+                            class="E10-design-logo"
+                            width="230"
+                            height="40"
+                            alt="Maxvi"
+                        />
+                        <br />
+                        E10
+                    </div>
+                </div>
+
+                <div
+                    class="E10-media-container col-xs-12 col-sm-4 col-sm-offset-2"
+                >
+                    <figure class="E10-design-figure">
+                        <img
+                            loading="lazy"
+                            src="<?= $folder ?>/images/section-design/E10-design-phone-front.png"
+                            class="E10-design-media layer-1"
+                            width="304"
+                            height="601"
+                            alt="Мобильный телефон Maxvi E10 оранжевого цвета"
+                            data-depth="0.1"
+                        />
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="E10-screen">
+        <div class="container">
+            <div class="content-wrapper row d-flex">
+                <div class="E10-media-container col-xs-12 col-sm-4">
+                    <figure class="E10-screen-figure">
+                        <img
+                            loading="lazy"
+                            src="<?= $folder ?>/images/section-screen/E10-screen-phone-front.png"
+                            class="E10-screen-media"
+                            width="575"
+                            height="1223"
+                            alt="Мобильный телефон Maxvi E10"
+                            data-depth="0.1"
+                        />
+                    </figure>
+                </div>
+
+                <div
+                    class="E10-typography-container col-xs-12 col-sm-4"
+                >
+                    <div
+                        class="E10-screen-title"
+                        data-aos="fade-up"
+                        data-aos-duration="500"
+                    >
+                        ДВА
+                        <br />
+                        ЭКРАНА
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section
+        class="E10-battery section-dark"
+        style="
+            background-image: url('<?= $folder ?>/images/section-battery/E10-battery-bg.jpg');
+        "
+    >
+        <div class="container">
+            <div class="content-wrapper row d-flex">
+                <div
+                    class="E10-typography-container col-xs-12 col-sm-6"
+                >
+                    <div
+                        class="E10-battery-title"
+                        data-aos="fade-right"
+                    >
+                        ЁМКИЙ
+                        <br />
+                        АККУМУЛЯТОР
+                        <br />
+                        <span class="text-large">2000</span>
+                        <span class="text-small">mAh</span>
+                    </div>
+                </div>
+
+                <div class="E10-media-container col-xs-12 col-sm-6">
+                    <figure
+                        class="E10-battery-figure"
+                        data-aos="fade-left"
+                        data-aos-duration="800"
+                    >
+                        <img
+                            loading="lazy"
+                            src="<?= $folder ?>/images/section-battery/E10-battery-phone-back.png"
+                            class="E10-battery-media"
+                            width="572"
+                            height="958"
+                            alt="Мобильный телефон Maxvi E10"
+                        />
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="E10-colors">
+        <div class="container">
+            <div class="content-wrapper row d-flex">
+                <div class="E10-typography-container col-xs-12">
+                    <div class="E10-colors-title">ВЫБЕРИ СВОЙ ЦВЕТ</div>
+                </div>
+
+                <div
+                    class="E10-media-container col-xs-12 col-sm-4 col-sm-offset-4"
+                >
+                    <figure
+                        class="E10-colors-figure E10-colors-figure--red"
+                        data-aos="fade-left"
+                        data-aos-anchor=".E10-colors-figure--black"
+                        data-aos-delay="600"
+                        data-aos-duration="800"
+                    >
+                        <img
+                            loading="lazy"
+                            src="<?= $folder ?>/images/section-colors/E10-colors-phone-red-back.png"
+                            class="E10-colors-media"
+                            width="428"
+                            height="581"
+                            alt="Мобильный телефон Maxvi E10 красного цвета"
+                        />
+
+                        <figcaption>КРАСНЫЙ</figcaption>
+                    </figure>
+
+                    <figure
+                        class="E10-colors-figure E10-colors-figure--black"
+                        data-aos="fade-left"
+                        data-aos-duration="800"
+                    >
+                        <img
+                            loading="lazy"
+                            src="<?= $folder ?>/images/section-colors/E10-colors-phone-black-back.png"
+                            class="E10-colors-media"
+                            width="428"
+                            height="581"
+                            alt="Мобильный телефон Maxvi E10 черного цвета"
+                        />
+                        <figcaption>ЧЕРНЫЙ</figcaption>
+                    </figure>
+
+                    <figure
+                        class="E10-colors-figure E10-colors-figure--orange"
+                        data-aos="fade-right"
+                        data-aos-anchor=".E10-colors-figure--black"
+                        data-aos-delay="600"
+                        data-aos-duration="800"
+                    >
+                        <img
+                            loading="lazy"
+                            src="<?= $folder ?>/images/section-colors/E10-colors-phone-orange-back.png"
+                            class="E10-colors-media"
+                            width="428"
+                            height="581"
+                            alt="Мобильный телефон Maxvi E10 оранжевого цвета"
+                        />
+                        <figcaption>ОРАНЖЕВЫЙ</figcaption>
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
