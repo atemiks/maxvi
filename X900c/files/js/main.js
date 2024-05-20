6dfd17b4ee1e1c1ec74ec5ba2b4f262e750fec0a
atemiks <atemiks@gmail.com>
2024-05-20 22:54:01 +0300

    Maxvi x900c

---
+++ X900c/files/js/main.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+    /* Animation */
+    AOS.init({
+        anchorPlacement: 'bottom-bottom',
+    });
+
+    /* Parallax */
+    const introParallax = document.querySelector('.X900c-intro-parallax');
+    new Parallax(introParallax);
+
+    const screenParallax = document.querySelector('.X900c-screen-parallax');
+    new Parallax(screenParallax);
+
+    // /* ScrollTrigger */
+    const screenAndCaseSection = document.querySelector('.X900c-screen-and-case-presentation');
+    const screenFigure = document.querySelector('.X900c-screen-figure');
+    const mm = gsap.matchMedia();
+
+    mm.add({
+        isDesktop: '(min-width: 1200px)',
+        isLaptop: '(min-width: 992px) and (max-width: 1199.98px)',
+        isTablet: '(min-width: 768px) and (max-width: 991.98px)',
+        isPhone: '(max-width: 767.98px)'
+    }, (context) => {
+        const { isPhone } = context.conditions;
+    
+        const screenAndCaseTl = gsap.timeline({
+            scrollTrigger: {
+                trigger: screenAndCaseSection,
+                markers: false,
+                start: isPhone ? '5% 5%' : '0 5%',
+                end: isPhone ? '55% 5%' : '50% 5%',
+                scrub: 0.1,
+                pin: screenFigure,
+                pinSpacing: false,
+                snap: {
+                    snapTo: 'labels',
+                    duration: { min: 0.2, max: 1 },
+                    delay: 0.2,
+                    ease: 'power1.inOut',
+                },
+            },
+        });
+    
+        screenAndCaseTl.addLabel('start')
+            .to(screenFigure, {
+                xPercent: isPhone ? 0 : 175,
+                rotate: -15,
+            });
+        screenAndCaseTl.addLabel('end');
+    
+        const batteryAndPowerbankSection = document.querySelector('.X900c-battery-and-powerbank-presentation');
+        const batterySection = document.querySelector('.X900c-battery')
+        const batteryVideo = document.querySelector('.X900c-battery-video');
+        const batteryFigure = document.querySelector('.X900c-battery-figure');
+    
+        const batteryAndPowerbankTl = gsap.timeline({
+            scrollTrigger: {
+                trigger: batteryAndPowerbankSection,
+                markers: true,
+                start: '0 0%',
+                end: '100% 100%',
+                scrub: 0.1,
+                pin: batteryVideo,
+                pinSpacing: false,
+                snap: {
+                    snapTo: 'labels',
+                    duration: { min: 0.2, max: 1 },
+                    delay: 0.2,
+                    ease: 'power1.inOut',
+                },
+            },
+        });
+    
+        batteryAndPowerbankTl.addLabel('start')
+            .to(batteryFigure, {
+                top: batterySection.clientHeight
+            });
+        batteryAndPowerbankTl.addLabel('end');
+    });
+});
---
+++ X900c/include.php
@@ -0,0 +1,363 @@
+<?php
+
+/**
+ * Bitrix vars
+ *
+ * @var array $templateFolder
+ * @var array $arParams
+ * @var CMain $APPLICATION
+ * @var CUser $USER
+ */
+
+$folder = substr(__DIR__, strlen($_SERVER['DOCUMENT_ROOT'])) . '/files/';
+
+// Bootstrap 3.4.1
+$this->addExternalCss("https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css");
+
+//AOS
+$this->addExternalCss(SITE_TEMPLATE_PATH . "/css/aos.css");
+$this->addExternalJS(SITE_TEMPLATE_PATH . '/js/aos.js');
+
+//Styles
+$this->addExternalCss($folder . "css/style.css");
+$this->addExternalCss($folder . "css/style-responsive.css");
+
+//parallax
+$this->addExternalJS(SITE_TEMPLATE_PATH . '/js/parallax.min.js');
+
+// Анимация
+$this->addExternalJS(SITE_TEMPLATE_PATH . '/js/gsap3/gsap-3.12.2.min.js');
+$this->addExternalJS(SITE_TEMPLATE_PATH . '/js/gsap3/ScrollTrigger.js');
+
+//Scripts
+$this->addExternalJS($folder . 'js/main.js');
+
+?>
+<div class="X900c-landing">
+    <section
+        class="X900c-intro"
+        style="
+            background-image: url('<?= $folder ?>/images/section-intro/X900c-intro-bg.jpg');
+        "
+    >
+        <div class="container">
+            <div class="content-wrapper row d-flex">
+                <div
+                    class="X900c-typography-container col-xs-12 col-sm-5"
+                >
+                    <div
+                        class="X900c-intro-model"
+                        data-aos="fade-right"
+                        data-aos-duration="500"
+                    >
+                        <img
+                            loading="lazy"
+                            src="<?= $folder ?>/images/section-intro/logo-maxvi.svg"
+                            class="X900c-intro-logo"
+                            width="205"
+                            height="35"
+                            alt="Maxvi"
+                        />
+                        X900c
+                    </div>
+
+                    <div
+                        class="X900c-intro-title"
+                        data-aos="fade-right"
+                        data-aos-duration="500"
+                    >
+                        Стильная
+                        <br />
+                        классика
+                    </div>
+                </div>
+
+                <div class="X900c-media-container col-xs-12 col-sm-7">
+                    <figure
+                        class="X900c-intro-figure"
+                        data-aos="fade-left"
+                        data-aos-duration="800"
+                    >
+                        <div class="X900c-intro-parallax">
+                            <img
+                                data-depth="0.1"
+                                loading="lazy"
+                                src="<?= $folder ?>/images/section-intro/X900c-intro-phone-front.png"
+                                class="X900c-intro-media"
+                                width="399"
+                                height="742"
+                                alt="Мобильный телефон Maxvi X900c"
+                            />
+                        </div>
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="X900c-contacts">
+        <div class="container">
+            <div class="content-wrapper row d-flex">
+                <div class="X900c-media-container col-xs-12 col-sm-3">
+                    <figure
+                        class="X900c-contacts-woman"
+                        data-aos="fade-right"
+                        data-aos-duration="500"
+                        style="
+                            background-image: url('<?= $folder ?>/images/section-contacts/X900c-contacts-bg.jpg');
+                        "
+                    ></figure>
+                </div>
+
+                <div
+                    class="X900c-media-container col-xs-12 col-xs-8 col-sm-4 col-xs-offset-4 col-sm-offset-0"
+                >
+                    <figure
+                        class="X900c-contacts-figure"
+                        data-aos="fade-up"
+                        data-aos-duration="800"
+                    >
+                        <img
+                            loading="lazy"
+                            src="<?= $folder ?>/images/section-contacts/X900c-contacts-phone-front.png"
+                            class="X900c-contacts-media"
+                            width="293"
+                            height="707"
+                            alt="Мобильный телефон Maxvi X900c"
+                        />
+                    </figure>
+                </div>
+
+                <div
+                    class="X900c-typography-container col-xs-12 col-sm-5"
+                >
+                    <div
+                        class="X900c-contacts-title"
+                        data-aos="fade-left"
+                        data-aos-duration="500"
+                    >
+                        Фото и мелодия
+                        <br />
+                        для каждого
+                        <br />
+                        контакта
+                    </div>
+
+                    <div
+                        class="X900c-contacts-subtitle"
+                        data-aos="fade-left"
+                        data-aos-duration="500"
+                    >
+                        Теперь еще
+                        <br />
+                        комфортнее звонить
+                        <br />
+                        и принимать вызовы
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="X900c-screen-and-case-presentation">
+        <section class="X900c-screen section-dark">
+            <video
+                class="X900c-screen-video"
+                poster="<?= $folder ?>/images/section-screen/X900c-screen-video-poster.jpg"
+                autoplay
+                muted
+                playsinline
+                loop
+            >
+                <source
+                    src="<?= $folder ?>/images/section-screen/X900c-screen-video.mp4"
+                    type="video/mp4"
+                />
+            </video>
+
+            <div class="container">
+                <div class="content-wrapper row d-flex">
+                    <div
+                        class="X900c-media-container col-xs-12 col-sm-5 col-md-4"
+                    >
+                        <figure class="X900c-screen-figure">
+                            <div
+                                class="X900c-screen-aos"
+                                data-aos="fade-right"
+                                data-aos-duration="800"
+                            >
+                                <div class="X900c-screen-parallax">
+                                    <img
+                                        data-depth="0.1"
+                                        loading="lazy"
+                                        src="<?= $folder ?>/images/section-screen/X900c-screen-phone-front.png"
+                                        class="X900c-screen-media"
+                                        width="344"
+                                        height="765"
+                                        alt="Мобильный телефон Maxvi X900c"
+                                    />
+                                </div>
+                            </div>
+                        </figure>
+                    </div>
+
+                    <div
+                        class="X900c-typography-container col-xs-12 col-sm-5 col-md-4 col-sm-offset-2 col-md-offset-4"
+                    >
+                        <div
+                            class="X900c-screen-title"
+                            data-aos="fade-left"
+                            data-aos-duration="500"
+                        >
+                            Большой
+                            <br />
+                            экран
+                            <br />
+                            <b>2.8’’</b>
+                        </div>
+
+                        <div
+                            class="X900c-screen-subtitle"
+                            data-aos="fade-left"
+                            data-aos-duration="500"
+                        >
+                            Любимые видео
+                            <br />
+                            и фото всегда
+                            <br />
+                            с собой
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section
+            class="X900c-case"
+            style="
+                background-image: url('<?= $folder ?>/images/section-case/X900c-case-bg.jpg');
+            "
+        >
+            <div class="container">
+                <div class="content-wrapper row d-flex">
+                    <div
+                        class="X900c-typography-container col-xs-12 col-sm-6"
+                    >
+                        <div
+                            class="X900c-case-title"
+                            data-aos="fade-right"
+                            data-aos-duration="500"
+                        >
+                            Корпус с
+                            <br />
+                            металлической
+                            <br />
+                            рамкой
+                        </div>
+
+                        <div
+                            class="X900c-case-subtitle"
+                            data-aos="fade-right"
+                            data-aos-duration="500"
+                        >
+                            делает телефон
+                            <br />
+                            ещё прочнее
+                            <br />
+                            и долговечнее
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </section>
+
+    <section class="X900c-battery-and-powerbank-presentation">
+        <section class="X900c-battery section-dark">
+            <video
+                class="X900c-battery-video"
+                poster="<?= $folder ?>/images/section-battery/X900c-battery-video-poster.jpg"
+                autoplay
+                muted
+                playsinline
+                loop
+            >
+                <source
+                    src="<?= $folder ?>/images/section-battery/X900c-battery-video.mp4"
+                    type="video/mp4"
+                />
+            </video>
+
+            <div class="container">
+                <div class="content-wrapper row d-flex">
+                    <div
+                        class="X900c-typography-container col-xs-12 col-sm-6"
+                    >
+                        <div
+                            class="X900c-battery-title"
+                            data-aos="fade-right"
+                            data-aos-duration="500"
+                        >
+                            Ёмкая батарея
+                            <br />
+                            2500 мАч
+                        </div>
+
+                        <div
+                            class="X900c-battery-subtitle"
+                            data-aos="fade-right"
+                            data-aos-duration="500"
+                        >
+                            длительная автономная
+                            <br />
+                            работа телефона
+                        </div>
+                    </div>
+
+                    <div class="X900c-media-container col-xs-12">
+                        <figure class="X900c-battery-figure">
+                            <img
+                                loading="lazy"
+                                src="<?= $folder ?>/images/section-battery/X900c-battery-phone-front.png"
+                                class="X900c-battery-media"
+                                width="856"
+                                height="459"
+                                alt="Мобильный телефон Maxvi X900c"
+                            />
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="X900c-powerbank section-dark">
+            <div class="container">
+                <div class="content-wrapper row d-flex">
+                    <div
+                        class="X900c-typography-container col-xs-9 col-sm-7 col-xs-offset-3 col-sm-offset-5"
+                    >
+                        <div
+                            class="X900c-powerbank-title"
+                            data-aos="fade-left"
+                            data-aos-duration="500"
+                            data-aos-anchor-placement="bottom-bottom"
+                        >
+                            Power bank
+                            <br />
+                            с разъёмом Type–C
+                        </div>
+
+                        <div
+                            class="X900c-powerbank-subtitle"
+                            data-aos="fade-left"
+                            data-aos-duration="500"
+                            data-aos-anchor-placement="bottom-bottom"
+                        >
+                            делитесь энергией
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </section>
+</div>
